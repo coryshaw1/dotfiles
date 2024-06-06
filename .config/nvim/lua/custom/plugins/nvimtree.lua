fdd976fbbf18d3044ad1d21b8f4754184f02ab4b
Cory Shaw <cory.shaw.dev@gmail.com>
2024-06-06 10:14:49 -0400

    update(nvim-tree): Change nvim-tree pane to open up the tree to the current file in the buffer when toggled open

--- .config/nvim/lua/custom/plugins/nvimtree.lua
+++ .config/nvim/lua/custom/plugins/nvimtree.lua
@@ -2,6 +2,5 @@ return {
   "nvim-tree/nvim-tree.lua",
   config = function()
     require("nvim-tree").setup()
-    vim.api.nvim_set_keymap("n", "<C-b>", ":NvimTreeToggle<CR>", { noremap = true })
   end,
 }
