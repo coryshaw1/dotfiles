a938d8bc5f831154e173e7d335ea408b6d445593
Cory Shaw <cory.shaw.dev@gmail.com>
2024-12-27 16:01:33 -0500

    fix(neoscroll): Specify exactly which movements to smooth scroll

--- .config/nvim/lua/custom/plugins/neoscroll.lua
+++ .config/nvim/lua/custom/plugins/neoscroll.lua
@@ -1,6 +1,6 @@
 return {
   "karb94/neoscroll.nvim",
   config = function()
-    require("neoscroll").setup {}
+    require("neoscroll").setup { mappings = { "<C-u>", "<C-d>", "zt", "zz", "zb" } }
   end,
 }
