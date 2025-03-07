cc26c8a2fa000af7552d0b724453d5253c5de78e
Cory Shaw <cory.shaw.dev@gmail.com>
2025-03-07 16:04:51 -0500

    fix(multigrep): Fix duplicate keymap after switching to Snacks picker

--- .config/nvim/lua/custom/multigrep.lua
+++ .config/nvim/lua/custom/multigrep.lua
@@ -50,7 +50,7 @@ local live_multigrep = function(opts)
 end
 
 M.setup = function()
-  vim.keymap.set("n", "<leader>fg", live_multigrep)
+  -- vim.keymap.set("n", "<leader>fg", live_multigrep)
 end
 
 return M
