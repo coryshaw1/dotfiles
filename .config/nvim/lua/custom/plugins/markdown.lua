b217c028e282ab15c0a161c9aaf16650965946df
Cory Shaw <cory.shaw.dev@gmail.com>
2025-03-07 16:02:57 -0500

    fix(avante): Fix markdown support for chat

--- .config/nvim/lua/custom/plugins/markdown.lua
+++ .config/nvim/lua/custom/plugins/markdown.lua
@@ -12,7 +12,8 @@ return {
     dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
-    opts = {},
+    opts = { file_types = { "markdown", "Avante" } },
+    ft = { "markdown", "Avante" },
   },
   {
     "gaoDean/autolist.nvim",
