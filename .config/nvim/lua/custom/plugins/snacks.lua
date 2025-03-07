15bb945e632ed99f6af67f286d55ada2f253da1e
Cory Shaw <cory.shaw.dev@gmail.com>
2025-03-07 16:01:47 -0500

    feat(snacks): Replace todo comments, telescope (where it can be supported), image, nvimtree, and indentations with snacks
    Telescope is still needed for neoclip, noice messages, and persisted session searching

--- .config/nvim/lua/custom/plugins/snacks.lua
+++ .config/nvim/lua/custom/plugins/snacks.lua
@@ -1,7 +1,29 @@
+local excluded = {
+  "node_modules/",
+  "dist/",
+  ".obsidian/",
+  ".next/",
+  ".vite/",
+  ".git/",
+  ".gitlab/",
+  "build/",
+  "target/",
+  "dadbod_ui/tmp/",
+  "dadbod_ui/dev/",
+
+  "package-lock.json",
+  "pnpm-lock.yaml",
+  "yarn.lock",
+  ".DS_Store",
+}
+
 return {
   "folke/snacks.nvim",
   priority = 1000,
   lazy = false,
+  dependencies = {
+    "folke/todo-comments.nvim",
+  },
   ---@module "snacks"
   ---@type snacks.Config
   opts = {
@@ -17,15 +39,30 @@ return {
         -- When using a function, the `items` argument are the default keymaps.
         ---@type snacks.dashboard.Item[]
         keys = {
-          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          {
+            icon = " ",
+            key = "f",
+            desc = "Find File",
+            action = ":lua Snacks.dashboard.pick('files', { follow = true, hidden = true })",
+          },
           { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-          { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
-          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          {
+            icon = " ",
+            key = "g",
+            desc = "Find Text",
+            action = ":lua Snacks.dashboard.pick('live_grep', { follow = true, hidden = true })",
+          },
+          {
+            icon = " ",
+            key = "r",
+            desc = "Recent Files",
+            action = ":lua Snacks.dashboard.pick('oldfiles', { follow = true, hidden = true })",
+          },
           {
             icon = " ",
             key = "c",
             desc = "Config",
-            action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+            action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config'), follow = true, hidden = true })",
           },
           { icon = " ", key = "g", desc = "Restore Session", action = ":SessionLoadLast" },
           { icon = "", key = "s", desc = "List Sessions", action = ":Telescope persisted" },
@@ -73,11 +110,23 @@ return {
         { section = "startup" },
       },
     },
-    indent = { enabled = true },
-    lazygit = { enabled = true },
+    image = {
+      doc = {
+        enabled = true,
+        float = true,
+        inline = false,
+      },
+    },
+    indent = {},
+    lazygit = {},
     notifier = {
       enabled = true,
       style = "fancy",
+    },
+    ---@class snacks.picker.Config
+    picker = {
+      hidden = true,
+      exclude = excluded,
     },
     explorer = {},
     ---@class snacks.statuscolumn.Config
@@ -96,12 +145,43 @@ return {
       refresh = 50, -- refresh at most every 50ms
     },
   },
+  -- stylua: ignore
   keys = {
-    {
-      "<C-b>",
-      function()
-        Snacks.picker.explorer()
-      end,
-    },
+    -- picker
+    { "<leader>fk", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
+    { "<leader><space>", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
+    { "<C-P>", function() Snacks.picker.files({ follow = true, hidden = true }) end, desc = "Find Files" },
+    { "<leader>ff", function() Snacks.picker.files({ follow = true, hidden = true}) end, desc = "Find Files" },
+    { "<leader>fc", function() Snacks.picker.files({ dirs = {vim.fn.stdpath("config")}, follow = true, hidden = true }) end, desc = "Find Config File" },
+    { "<leader>fg", function() Snacks.picker.grep({ follow = true }) end, desc = "Find Files", },
+    { "<leader>fb", function() Snacks.picker.buffers() end, desc = "Find Files" },
+    { "<leader>fh", function() Snacks.picker.help() end, desc = "Help" },
+    { "<leader>fd", function() Snacks.picker.diagnostics() end, desc = "Diagnostics" },
+    { "<leader>:", function() Snacks.picker.command_history() end, desc = "Command History" },
+    { "<leader>fz", function() Snacks.picker.zoxide() end, desc = "Zoxide" },
+    { "<leader>ft", function() Snacks.picker.todo_comments() end, desc = "Todo" },
+    -- search
+    { "<leader>sd", function() Snacks.picker.diagnostics() end, desc = "Diagnostics" },
+    { "<leader>si", function() Snacks.picker.icons() end, desc = "Icons" },
+    { "<leader>sj", function() Snacks.picker.jumps() end, desc = "Jumps" },
+    { "<leader>sk", function() Snacks.picker.keymaps() end, desc = "Keymaps" },
+    { "<leader>sc", function() Snacks.picker.colorschemes() end, desc = "Colorschemes" },
+    -- LSP
+    { "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition" },
+    { "gD", function() Snacks.picker.lsp_declarations() end, desc = "Goto Declaration" },
+    { "gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
+    { "<leader>fr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
+    { "gI", function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation" },
+    { "gy", function() Snacks.picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
+    { "<leader>ss", function() Snacks.picker.lsp_symbols() end, desc = "LSP Symbols" },
+    { "<leader>sS", function() Snacks.picker.lsp_workspace_symbols() end, desc = "LSP Workspace Symbols" },
+
+    -- explorer
+    { "<C-b>", function() Snacks.picker.explorer() end, desc = "Filetree Explorer"},
+
+    -- git
+    { "<leader>lg", function() Snacks.lazygit() end, desc = "LazyGit" },
+    { "<leader>gl", function() Snacks.lazygit.log() end, desc = "Git Log" },
+    { "<leader>gb", function() Snacks.picker.git_branches() end, desc = "Git Branches" },
   },
 }
--- .config/nvim/lua/custom/telescope.lua
+++ .config/nvim/lua/custom/telescope.lua
@@ -68,27 +68,27 @@ pcall(require("telescope").load_extension, "persisted")
 
 local builtin = require "telescope.builtin"
 
-vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
-vim.keymap.set("n", "<C-P>", builtin.find_files, { desc = "Telescope find files" })
--- vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope grep file contents" })
-vim.keymap.set("n", "<leader>fs", builtin.grep_string, { desc = "Telescope grep for string" })
-vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope find buffers" })
-vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope find help tags" })
-vim.keymap.set("n", "<leader>fr", builtin.lsp_references, { desc = "Telescope find references" })
-vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "Telescope find diagnostics" })
-vim.keymap.set("n", "gr", builtin.lsp_references, { desc = "LSP: Find references" })
-vim.keymap.set("n", "gi", builtin.lsp_implementations, { desc = "LSP: Find references" })
-vim.keymap.set("n", "<C-_>", builtin.current_buffer_fuzzy_find, { desc = "Telescope find in buffer" })
+-- vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+-- vim.keymap.set("n", "<C-P>", builtin.find_files, { desc = "Telescope find files" })
+-- -- vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope grep file contents" })
+-- vim.keymap.set("n", "<leader>fs", builtin.grep_string, { desc = "Telescope grep for string" })
+-- vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope find buffers" })
+-- vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope find help tags" })
+-- vim.keymap.set("n", "<leader>fr", builtin.lsp_references, { desc = "Telescope find references" })
+-- vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "Telescope find diagnostics" })
+-- vim.keymap.set("n", "gr", builtin.lsp_references, { desc = "LSP: Find references" })
+-- vim.keymap.set("n", "gi", builtin.lsp_implementations, { desc = "LSP: Find references" })
+-- vim.keymap.set("n", "<C-_>", builtin.current_buffer_fuzzy_find, { desc = "Telescope find in buffer" })
 vim.keymap.set("n", "<C-m>", ":Telescope neoclip a<cr>", { desc = "Telescope neoclip" })
-vim.keymap.set("n", "<leader>ft", ":TodoTelescope<cr>", { desc = "Find todos" })
+-- vim.keymap.set("n", "<leader>ft", ":TodoTelescope<cr>", { desc = "Find todos" })
 vim.keymap.set("n", "<leader>fm", ":Telescope noice<cr>", { desc = "Telescope messages (via noice)" })
 vim.keymap.set("n", "<leader>fp", ":Telescope persisted<cr>", { desc = "Telescope sessions (via persisted)" })
 
-vim.keymap.set("n", "<leader>fa", function()
-  ---@diagnostic disable-next-line: param-type-mismatch
-  builtin.find_files { cwd = vim.fs.joinpath(vim.fn.stdpath "data", "lazy") }
-end, { desc = "Telescope file search in lazy dir" })
-
-vim.keymap.set("n", "<leader>en", function()
-  builtin.find_files { cwd = vim.fn.stdpath "config" }
-end, { desc = "Telescope file search in config dotfiles" })
+-- vim.keymap.set("n", "<leader>fa", function()
+--   ---@diagnostic disable-next-line: param-type-mismatch
+--   builtin.find_files { cwd = vim.fs.joinpath(vim.fn.stdpath "data", "lazy") }
+-- end, { desc = "Telescope file search in lazy dir" })
+--
+-- vim.keymap.set("n", "<leader>en", function()
+--   builtin.find_files { cwd = vim.fn.stdpath "config" }
+-- end, { desc = "Telescope file search in config dotfiles" })
