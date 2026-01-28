838286def4cbdb21a1f14443305ea1b32f53380c
Cory Shaw <cory.shaw.dev@gmail.com>
2026-01-28 09:31:11 -0500

    fix(lsp): Fix lua_ls lsp config

--- .config/nvim/lua/custom/plugins/lsp.lua
+++ .config/nvim/lua/custom/plugins/lsp.lua
@@ -27,7 +27,27 @@ return {
         bashls = true,
         csharp_ls = true,
         gopls = true,
-        lua_ls = true,
+        lua_ls = {
+          settings = {
+            Lua = {
+              runtime = {
+                version = "LuaJIT",
+              },
+              diagnostics = {
+                globals = {
+                  "vim",
+                  "require",
+                },
+              },
+              workspace = {
+                library = vim.api.nvim_get_runtime_file("", true),
+              },
+              telemetry = {
+                enable = false,
+              },
+            },
+          },
+        },
         cssls = true,
         html = true,
         eslint = true,
