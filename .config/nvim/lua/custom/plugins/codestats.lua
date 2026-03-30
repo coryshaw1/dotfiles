384b75684b5faf042719e6ad7f6ac28d47689906
Cory Shaw <cory.shaw.dev@gmail.com>
2026-03-30 12:38:21 -0400

    update(codestats): Use environment variables

--- .config/nvim/lua/custom/plugins/codestats.lua
+++ .config/nvim/lua/custom/plugins/codestats.lua
@@ -1,9 +1,11 @@
 return {
   "YannickFricke/codestats.nvim",
   config = function()
-    require("codestats-nvim").setup {
-      token = "SFMyNTY.YldKemRYSm1aWEk9IyNNVFF4TkE9PQ.KbPfd0e6yhR-pVICb3Oi6brLGV-Howy6osd0W-f7XZk",
-    }
+    local token = vim.env.CODESTATS_API_KEY
+    if not token or token == "" then
+      return
+    end
+    require("codestats-nvim").setup { token = token }
   end,
   dependencies = { "nvim-lua/plenary.nvim" },
 }
