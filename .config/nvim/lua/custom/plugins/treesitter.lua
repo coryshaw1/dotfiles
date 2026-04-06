0fc194141d6335024e0c4622c6f6d1f0b5c57a99
Cory Shaw <cory.shaw.dev@gmail.com>
2026-04-06 08:56:30 -0400

    feat(treesitter): Add incremental and decremental selection bindings

--- .config/nvim/lua/custom/plugins/treesitter.lua
+++ .config/nvim/lua/custom/plugins/treesitter.lua
@@ -54,6 +54,23 @@ return {
         end,
       })
 
+      -- incremental selection
+      vim.keymap.set({ "n", "x", "o" }, "<c-Space>", function()
+        if vim.treesitter.get_parser(nil, nil, { error = false }) then
+          require("vim.treesitter._select").select_parent(vim.v.count1)
+        else
+          vim.lsp.buf.selection_range(vim.v.count1)
+        end
+      end, { desc = "Select parent treesitter node or outer incremental lsp selections" })
+
+      vim.keymap.set({ "n", "x", "o" }, "<c-i>", function()
+        if vim.treesitter.get_parser(nil, nil, { error = false }) then
+          require("vim.treesitter._select").select_child(vim.v.count1)
+        else
+          vim.lsp.buf.selection_range(-vim.v.count1)
+        end
+      end, { desc = "Select child treesitter node or inner incremental lsp selections" })
+
       require("nvim-ts-autotag").setup()
       require("mini.ai").setup()
     end,
