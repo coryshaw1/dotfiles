2cd7b9faf4547a5853ab934bbe509141574d80ff
Cory Shaw <cory.shaw.dev@gmail.com>
2026-04-24 09:25:26 -0400

    update(sesh): Add lazygit window for work sesh

--- .config/sesh/work_startup.sh
+++ .config/sesh/work_startup.sh
@@ -8,13 +8,16 @@ tmux rename-window dotfiles
 tmux send-keys "cd dotfiles" Enter
 tmux send-keys "clear" Enter
 
-tmux new-window -t work:2 -n Notes
+tmux new-window -t work:2 -n notes
 tmux select-window -t work:2
 tmux send-keys "cd obsidian" Enter
 tmux send-keys "clear" Enter
 
-tmux new-window -t work:3 -n zsh
-tmux select-window -t work:3
+tmux new-window -t work:3 -n git -c ~/dotfiles
+tmux send-keys -t work:3 "lazygit" Enter
+
+tmux new-window -t work:4 -n zsh
+tmux select-window -t work:4
 tmux send-keys "clear" Enter
 
 tmux select-window -t work:1
